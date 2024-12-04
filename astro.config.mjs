1faa2183b356edad8bff7a162abcdfa1b41d79b5
kimagin <info@kimagination.net>
2024-12-04 22:50:18 +0100

    Standard Base

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+
+import tailwind from '@astrojs/tailwind'
+import alpinejs from '@astrojs/alpinejs'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), alpinejs()],
+  devToolbar: {
+    enabled: false,
+  },
+})
+
